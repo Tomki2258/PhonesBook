127d03493d58cc4ccaba496ddf30633619b8a26a
tamus <52979692+Tomki2258@users.noreply.github.com>
2025-05-30 08:59:49 +0200

    Dodaj pliki projektów.

---
+++ PhonesBook/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PhonesBook.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
